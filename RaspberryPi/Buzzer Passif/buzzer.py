b2835f728e74ca387a4ee505f86eeabc3e45b49d
IuNoWat <51018773+IuNoWat@users.noreply.github.com>
2025-07-15 15:47:52 +0200

    update of buzzer

--- RaspberryPi/Buzzer Passif/buzzer.py
+++ RaspberryPi/Buzzer Passif/buzzer.py
@@ -1,21 +1,30 @@
+#!/usr/bin/python
+#coding: utf-8
 
+#IMPORTANT
+#PIGPOID NEED TO BE LAUNCHED BEFORE IMPORTING BUZZER.PY
+#VIA sudo pigpiod
 
 import time
 import os
+from threading import Thread
 
 import pigpio
 
+import music_bib
+
 #CONSTANTS
-os.system("sudo pigpoid")
+#os.system("sudo pigpiod") #pigpoid needs to be launched
 PIN=18
 PI=pigpio.pi()
-DEFAULT_OCTAVE=6
-DEFAULT_DUTY = 50000
+DEFAULT_OCTAVE= 6
+DEFAULT_DUTY = 50000 # 
 
 #INIT
 PI.set_mode(PIN,pigpio.OUTPUT)
 
 #NOTES
+#Frequencies used to play note, available on 8 octaves
 notes={
 "C":[16,32,65,130,261,523,1046,2093,4186,],
 "C#":[17,34,69,138,277,554,1108,2217,4434,],
@@ -32,36 +41,67 @@ notes={
 " ":[0]
 }
 
-#CLASS
+#EXAMPLE MUSIC
+#A music consists of a list of dict, each containing a note, an octave, and a time multiplied by the interval defined in play_music()
+#The " " key is silent
+default_music=[
+{"note":"D","oct":5,"time":1},
+{"note":"D","oct":5,"time":1},
+{"note":"D","oct":6,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"A","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"G#","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"G","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"F","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"D","oct":5,"time":1},
+{"note":"F","oct":5,"time":1},
+{"note":"G","oct":5,"time":1}
+]
 
-class Music() :
-    def __init__(self,interval=0.1) :
-        self.interval=interval
-
-time_note=0.1
-
-
-
-def play_note(note,duree,oct=DEFAULT_OCTAVE,duty=DEFAULT_DUTY) :
+def play_note(note,duree,oct=DEFAULT_OCTAVE,duty=DEFAULT_DUTY) : #To play a singular note for the duration of duree
     PI.hardware_PWM(PIN,notes[note][oct],duty)
     time.sleep(duree)
     PI.hardware_PWM(PIN,0,duty)
 
-def play_music(music) :
+def play_music(music=default_music,base_interval=0.15,duty=DEFAULT_DUTY) : #To play a music as described in DEFAULT_MUSIC
     for i,note in enumerate(music) :
-        play_note(note["note"],note["time"],note["oct"])
+        play_note(note["note"],base_interval*note["time"],note["oct"],duty)
 
-
-play_music(zelda)
-
-def play_all_notes(interval) :
+def play_all_notes(interval) : #Play all available notes
     for i in range(0,9) :
         for key in notes :
-            print(f"Playing note {key} at octave {i} : {notes[key][i]}")
-            PI.hardware_PWM(PIN,notes[key][i],500000)
-            time.sleep(interval)
+            if key!=" " :
+                print(f"Playing note {key} at octave {i} : {notes[key][i]}")
+                PI.hardware_PWM(PIN,notes[key][i],500000)
+                time.sleep(interval)
+            else :
+                print(f"Playing note {key} at octave {i} : {notes[key][0]}")
+                PI.hardware_PWM(PIN,notes[key][0],500000)
+                time.sleep(interval)
     PI.hardware_PWM(PIN,0,500000)
+            
+class Sound(Thread) :
+    def __init__(self,sound,interval=0.15,duty=DEFAULT_DUTY) :
+        Thread.__init__(self)
+        self.on=True
+        self.sound=sound
+        self.interval=interval
+        self.duty=duty
+    def run(self) :
+        play_music(self.sound,self.interval,self.duty)
 
+if __name__=="__main__" :
+    play_music()
 
-#play_all_notes(0.1)
+    #play_all_notes(0.1)
 
+    #my_sound=Sound(default_music)
+    #my_sound.start()
+    #for i in range(0,10) :
+    #    print(f"COUCOU {i}")
+    #    time.sleep(0.5)
---
+++ RaspberryPi/Buzzer Passif/music_bib.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#coding: utf-8
+
+#First notes of Megalovania
+mega=[
+{"note":"D","oct":5,"time":1},
+{"note":"D","oct":5,"time":1},
+{"note":"D","oct":6,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"A","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"G#","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"G","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"F","oct":5,"time":1},
+{"note":" ","oct":0,"time":1},
+{"note":"D","oct":5,"time":1},
+{"note":"F","oct":5,"time":1},
+{"note":"G","oct":5,"time":1}
+]
+
+#"Secret found" from zelda
+zelda=[
+{"note":"G","oct":5,"time":1},
+{"note":"F","oct":5,"time":1},
+{"note":"D","oct":5,"time":1},
+{"note":"A","oct":4,"time":1},
+{"note":"G","oct":4,"time":1},
+{"note":"E","oct":5,"time":1},
+{"note":"G","oct":5,"time":1},
+{"note":"C","oct":6,"time":1}
+]
