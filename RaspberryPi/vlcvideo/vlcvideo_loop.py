a28e7444f61527985f926b83e505a1ed5f695771
pi4_test <lorenzo.jacques@alsace.eu>
2025-07-30 11:13:58 +0100

    exit on "esc" in vlcvideo

--- RaspberryPi/vlcvideo/vlcvideo_loop.py
+++ RaspberryPi/vlcvideo/vlcvideo_loop.py
@@ -5,14 +5,23 @@ instance = vlc.Instance('--input-repeat=-1','--mouse-hide-timeout=0')
 player = instance.media_player_new()
 player.toggle_fullscreen()
 
-vid = vlc.Media("/home/vaisseau/Desktop/robot_nul.mp4")
+vid = vlc.Media("/home/pi/Desktop/robot_nul.mp4")
 
 player.set_media(vid)
 
-keyboard.add_hotkey("Esc",lambda:exit())
 
-while True :
-    if player.get_state() == vlc.State.Ended :
+playing=True
+
+def stop() :
+    global playing
+    playing=False
+    player.stop()
+
+
+keyboard.add_hotkey("Esc",lambda:stop())
+
+while playing :
+    if player.get_state() == vlc.State.Ended and playing :
         player.set_media(vid)
     player.play()
 
