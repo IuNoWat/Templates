5d4daab49d8254a8b7389c071de5e555bbd927cc
pi4_test <lorenzo.jacques@alsace.eu>
2025-07-30 10:56:52 +0100

    add exit to vlcvideo_loop

--- RaspberryPi/vlcvideo/vlcvideo_loop.py
+++ RaspberryPi/vlcvideo/vlcvideo_loop.py
@@ -1,4 +1,5 @@
 import vlc
+import keyboard
 
 instance = vlc.Instance('--input-repeat=-1','--mouse-hide-timeout=0')
 player = instance.media_player_new()
@@ -7,6 +8,8 @@ player.toggle_fullscreen()
 vid = vlc.Media("/home/vaisseau/Desktop/robot_nul.mp4")
 
 player.set_media(vid)
+
+keyboard.add_hotkey("Esc",lambda:exit())
 
 while True :
     if player.get_state() == vlc.State.Ended :
